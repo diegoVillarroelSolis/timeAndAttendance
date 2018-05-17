21ced756624c7f91b25d284b993f97d890cc4ada
Vanessa Sanjinez <vanessa.sanjinez@truextend.com>
2018-05-17 14:33:30 -0400

    Added project structure

---
+++ src/main/java/com/example/demo/TimeAndAttendanceServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeAndAttendanceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeAndAttendanceServerApplication.class, args);
+	}
+}
